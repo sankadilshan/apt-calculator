178d7f4b676687a1099fe54e1dc01a9db2237823
sankadilishan <sankadilishan9686@gmail.com>
2023-11-24 05:36:43 +0530

    improved logic

--- src/main/java/com/sankadilshan/taxcalculation/TaxServiceImpl.java
+++ src/main/java/com/sankadilshan/taxcalculation/TaxServiceImpl.java
@@ -3,43 +3,28 @@ package com.sankadilshan.taxcalculation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class TaxServiceImpl implements TaxService {
 
-    int[] percentage = new int[]{6, 12, 18, 24, 30, 36};
     private final Map<Integer, TaxResponse> taxMap = new HashMap<>();
+    List<Integer> percentage = Arrays.asList(6, 12, 18, 24, 30, 36);
     @Value("${tax.taxPartAmount}")
-    private double taxPartAmount;
+    private final double taxPartAmount = 41666.67;
 
     public FinalTaxResponse calculateTax(double amount, double taxFreeAmount) {
-        if (amount - taxFreeAmount < 0) {
-            taxMap.put(0, TaxResponse.builder().percentage(0).amount(0d).build());
-            return FinalTaxResponse.builder().taxResponse(taxMap).totalTax(0d).build();
-        }
-
-        double taxNoFreeAmount = amount - taxFreeAmount;
-        int percentageIndex = 0;
-        do {
-            double v = taxNoFreeAmount - taxPartAmount;
-            if (v < 0) {
-                calculation(percentageIndex, taxNoFreeAmount);
-                taxNoFreeAmount = 0;
-            } else if (v > 0) {
-                calculation(percentageIndex, taxPartAmount);
-                taxNoFreeAmount = v;
-                percentageIndex++;
-            }
-        } while (taxNoFreeAmount > 0);
-
+        taxMap.clear();
+        caluclateTax(amount, taxFreeAmount);
         return getEndResponse(taxMap);
     }
 
     private void calculation(int percentageIndex, double partAmount) {
-        double taxAmount = partAmount * percentage[percentageIndex] / 100;
-        taxMap.put(percentageIndex, TaxResponse.builder().percentage(percentage[percentageIndex]).amount(taxAmount).build());
+        double taxAmount = Math.round((partAmount * percentage.get(percentageIndex) / 100) * 100.00) / 100.00;
+        taxMap.put(percentageIndex, TaxResponse.builder().percentage(percentage.get(percentageIndex)).amount(taxAmount).build());
     }
 
     private FinalTaxResponse getEndResponse(Map<Integer, TaxResponse> taxMap) {
@@ -50,5 +35,34 @@ public class TaxServiceImpl implements TaxService {
         }
         double roundOff = Math.round(totalTax * 100.0) / 100.0;
         return FinalTaxResponse.builder().taxResponse(taxMap).totalTax(roundOff).build();
+    }
+
+    private void caluclateTax(double netAmount, double taxFreeAmount) {
+        taxMap.clear();
+
+        double amountNotRelief = netAmount - taxFreeAmount;
+        if (amountNotRelief > 0) {
+            for (int pr : percentage) {
+                if (amountNotRelief > 0) {
+                    if (pr == percentage.get(percentage.size() - 1)) {
+                        calculation2(pr, amountNotRelief);
+                    } else {
+                        double v = amountNotRelief - taxPartAmount;
+                        double selectedAmount = v > 0 ? taxPartAmount : amountNotRelief;
+                        calculation2(pr, selectedAmount);
+                        if (v > 0) {
+                            amountNotRelief = v;
+                        } else {
+                            amountNotRelief = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void calculation2(int pr, double partAmount) {
+        double taxAmount = Math.round((partAmount * pr / 100) * 100.00) / 100.00;
+        taxMap.put(percentage.indexOf(pr), TaxResponse.builder().percentage(pr).amount(taxAmount).build());
     }
 }
